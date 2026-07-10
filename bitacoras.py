66152835f2272db41df00e0cc65aaf154461cfac
TAMTARA <karenpbq@gmail.com>
2026-07-10 13:04:29 -0500

    Update bitacoras.py

--- bitacoras.py
+++ bitacoras.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 def mostrar(supervisor_id=None):
-    st.write("Módulo de bitácoras temporal en mantenimiento.")
+    st.info("🔄 El módulo de Bitácoras de Producción se encuentra temporalmente en mantenimiento.")
